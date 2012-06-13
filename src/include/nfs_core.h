830b6248451aa850fad6aad255411c957b466f87
Max Matveev <makc@gmx.co.uk>
2012-06-13 12:37:15 +1000

    Don't use FSAL-specific types in the common code
    Remove automatic propagation of FSAL-specific types into
    common code via fsal_types.h. This means that there is
    no longer definitions for things like fsal_handle_t or fsal_file_t:
    those things are all specific to the FSAL and should be accessed
    via FSAL API.
    
    Signed-off-by: Max Matveev <makc@gmx.co.uk>

--- src/include/nfs_core.h
+++ src/include/nfs_core.h
@@ -325,7 +325,7 @@ typedef struct nfs_param__
   hash_parameter_t nlm_client_hash_param;
   hash_parameter_t nlm_owner_hash_param;
 #endif
-  fsal_parameter_t fsal_param;
+  nfs_cache_layers_parameter_t cache_layers_param;
   external_tools_parameter_t extern_param;
 
   /* list of exports declared in config file */
