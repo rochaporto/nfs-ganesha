d62546ab901d4e34e1a30169515a2b856ceb8373
rochaporto <rocha.porto@gmail.com>
2012-07-25 17:52:28 +0200

    we probably wont need this file though.

--- src/include/FSAL/FSAL_DMLITE/fsal_handle_syscalls.h
+++ src/include/FSAL/FSAL_DMLITE/fsal_handle_syscalls.h
@@ -1,7 +1,4 @@
 /*
- *   Copyright (C) International Business Machines  Corp., 2010
- *   Author(s): Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>
- *
  *   This library is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU Lesser General Public License as published
  *   by the Free Software Foundation; either version 2.1 of the License, or
